8383b146978da50f5e172848c8070ea7f037fce8
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2018-11-26 10:44:56 +0200

    initial

---
+++ 01. Intro and Basic Syntax - Exercise/P05.Login/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace P05.Login
+{
+    using System;
+    class Startup
+    {
+        public static void Main()
+        {
+            string username = Console.ReadLine();
+
+            string password = string.Empty;
+
+            for (int i = username.Length - 1; i > -1; i--)
+            {
+                password += username[i];
+            }
+
+            for (int i = 1; i <= 4; i++)
+            {
+                string input = Console.ReadLine();
+                if (input == password)
+                {
+                    Console.WriteLine($"User {username} logged in.");
+                    return;
+                }
+
+                if (i == 4)
+                {
+                    Console.WriteLine($"User {username} blocked!");
+                    return;
+                }
+                Console.WriteLine("Incorrect password. Try again.");
+            }
+        }
+    }
+}
