380b1cfb167028e147da8aac3d9cd2247dbfe177
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2018-11-26 10:49:51 +0200

    initial

---
+++ 01. Intro and Basic Syntax - Exercise/P08.TriangleOfNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace P08.TriangleOfNumbers
+{
+    using System;
+    class Startup
+    {
+        public static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+            for (int row = 1; row <= number; row++)
+            {
+                for (int col = 1; col <= row; col++)
+                {
+                    Console.Write(row + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
