723b7f92119bdb847077ffeb6a5d311e06d32028
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2018-11-26 11:32:54 +0200

    initial

---
+++ 01. Intro-and-Basic-Syntax - Lab/P05.MonthPrinter/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace P05.MonthPrinter
+{
+    using System;
+    class Startup
+    {
+        public static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if (number > 12 || number < 1)
+            {
+                Console.WriteLine("Error!");
+            }
+            else
+            {
+                if (number == 1)
+                {
+                    Console.WriteLine("January");
+                }
+                else if (number == 2)
+                {
+                    Console.WriteLine("February");
+                }
+                else if (number == 3)
+                {
+                    Console.WriteLine("March");
+                }
+                else if (number == 4)
+                {
+                    Console.WriteLine("April");
+                }
+                else if (number == 5)
+                {
+                    Console.WriteLine("May");
+                }
+                else if (number == 6)
+                {
+                    Console.WriteLine("June");
+                }
+                else if (number == 7)
+                {
+                    Console.WriteLine("July");
+                }
+                else if (number == 8)
+                {
+                    Console.WriteLine("August");
+                }
+                else if (number == 9)
+                {
+                    Console.WriteLine("September");
+                }
+                else if (number == 10)
+                {
+                    Console.WriteLine("October");
+                }
+                else if (number == 11)
+                {
+                    Console.WriteLine("November");
+                }
+                else if (number == 12)
+                {
+                    Console.WriteLine("December");
+                }
+            }
+        }
+    }
+}
---
+++ 01. Intro-and-Basic-Syntax - Lab/P06.ForeignLanguages/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace P06.ForeignLanguages
+{
+    using System;
+    class Startup
+    {
+        static void Main()
+        {
+            string countryName = Console.ReadLine().ToLower();
+
+            if (countryName == "usa" || countryName == "england")
+            {
+                Console.WriteLine("English");
+            }
+            else if (countryName == "spain" || countryName == "argentina" || countryName == "mexico")
+            {
+                Console.WriteLine("Spanish");
+            }
+            else
+            {
+                Console.WriteLine("unknown");
+            }
+        }
+    }
+}
