95c5df31cd3c6ba2b33a89f49dac9c6a1b423a78
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2018-11-26 11:50:07 +0200

    initial

---
+++ 01. Intro-and-Basic-Syntax - Lab/P07.TheatrePromotions/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace P07.TheatrePromotions
+{
+    using System;
+
+    class Startup
+    {
+        public static void Main()
+        {
+            string dayOfWeek = Console.ReadLine().ToLower();
+            int age = int.Parse(Console.ReadLine());
+            int price = 0;
+            bool outOfRange = true;
+
+            if (dayOfWeek == "weekday")
+            {
+                if (age >= 0 && age <= 18)
+                {
+                    price = 12;
+                }
+                else if (age > 18 && age <= 64)
+                {
+                    price = 18;
+                }
+                else if (age > 64 && age <= 122)
+                {
+                    price = 12;
+                }
+                else
+                {
+                    outOfRange = false;
+                }
+            }
+            else if (dayOfWeek == "weekend")
+            {
+                if (age >= 0 && age <= 18)
+                {
+                    price = 15;
+                }
+                else if (age > 18 && age <= 64)
+                {
+                    price = 20;
+                }
+                else if (age > 64 && age <= 122)
+                {
+                    price = 15;
+                }
+                else
+                {
+                    outOfRange = false;
+                }
+            }
+            else if (dayOfWeek == "holiday")
+            {
+                if (age >= 0 && age <= 18)
+                {
+                    price = 5;
+                }
+                else if (age > 18 && age <= 64)
+                {
+                    price = 12;
+                }
+                else if (age > 64 && age <= 122)
+                {
+                    price = 10;
+                }
+                else
+                {
+                    outOfRange = false;
+                }
+            }
+            if (!outOfRange)
+            {
+                Console.WriteLine("Error!");
+            }
+            else
+            {
+                Console.WriteLine($"{price}$");
+            }
+        }
+    }
+}
---
+++ 01. Intro-and-Basic-Syntax - Lab/P08.DivisibleBy3/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace P08.DivisibleBy3
+{
+    using System;
+    class Startup
+    {
+        public static void Main()
+        {
+            for (int i = 3; i <= 100; i += 3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
