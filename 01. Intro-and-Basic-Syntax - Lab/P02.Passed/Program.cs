2b2630e7c33996a858c853f115a54a4471245770
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2018-11-26 11:14:17 +0200

    initial

---
+++ 01. Intro-and-Basic-Syntax - Lab/P02.Passed/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace P02.Passed
+{
+    using System;
+
+    class Program
+    {
+        public static void Main()
+        {
+            double grade = double.Parse(Console.ReadLine());
+            if (grade >= 3)
+            {
+                Console.WriteLine("Passed!");
+            }
+        }
+    }
+}
