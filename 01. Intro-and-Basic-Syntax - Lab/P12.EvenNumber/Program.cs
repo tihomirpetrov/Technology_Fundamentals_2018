a61603f319b0970f4be2a05002f700825d1f7ef3
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2018-11-26 11:59:47 +0200

    initial

---
+++ 01. Intro-and-Basic-Syntax - Lab/P12.EvenNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace P12.EvenNumber
+{
+    using System;
+    class Startup
+    {
+        public static void Main()
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            while (true)
+            {
+                if (input % 2 == 0)
+                {
+                    Console.WriteLine($"The number is: {Math.Abs(input)}");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine($"Please write an even number.");
+                }
+                input = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
