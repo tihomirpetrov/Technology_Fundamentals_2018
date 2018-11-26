4838769418ba26b07e31100fee06ae20f7edf0b3
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2018-11-26 15:35:13 +0200

    initial

---
+++ 03.Arrays - Exercise/P08.MagicSum/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace P08.MagicSum
+{
+    using System;
+    using System.Linq;
+    class Startup
+    {
+        public static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] + numbers[j] == n)
+                    {
+                        Console.WriteLine($"{numbers[i]} {numbers[j]}");
+                    }
+                }
+            }
+        }
+    }
+}
