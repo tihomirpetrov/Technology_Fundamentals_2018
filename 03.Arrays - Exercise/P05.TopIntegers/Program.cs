22a3e20c1dd912cfa251e9aff7c84fb7e89ba64a
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2018-11-26 15:29:15 +0200

    initial

---
+++ 03.Arrays - Exercise/P05.TopIntegers/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace P05.TopIntegers
+{
+    using System;
+    using System.Linq;
+    class Startup
+    {
+        public static void Main()
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int number = input[i];
+                for (int j = i + 1; j < input.Length; j++)
+                {
+                    int secondNumber = input[j];
+                    if (number <= secondNumber)
+                    {
+                        break;
+                    }
+                    else if (j == input.Length - 1)
+                    {
+                        Console.Write(number + " ");
+                    }
+                }
+            }
+            Console.WriteLine(input[input.Length - 1]);
+        }
+    }
+}
---
+++ 03.Arrays - Exercise/P06.EqualSum/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace P06.EqualSum
+{
+    using System;
+    using System.Linq;
+
+    class StartUp
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            if (numbers.Length == 1)
+            {
+                Console.WriteLine($"0");
+            }
+            else
+            {
+
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    int leftSum = 0;
+                    int rightSum = 0;
+                    for (int j = 0; j < i; j++)
+                    {
+                        leftSum += numbers[j];
+                    }
+                    for (int k = i + 1; k < numbers.Length; k++)
+                    {
+                        rightSum += numbers[k];
+                    }
+                    if (leftSum == rightSum)
+                    {
+                        Console.WriteLine(i);
+                        return;
+                    }
+                }
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
