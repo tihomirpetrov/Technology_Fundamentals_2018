ba0278321996b90c821c8ee4d69965eb43588cda
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2018-11-26 14:26:40 +0200

    initial

---
+++ 02.Data Types and Variables - Lab/P01.IntegerOperations/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace P01.IntegerOperations
+{
+    using System;
+    class Startup
+    {
+        public static void Main()
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            int fourthNumber = int.Parse(Console.ReadLine());
+
+            int result = (firstNumber + secondNumber) / thirdNumber * fourthNumber;
+            Console.WriteLine(result);
+        }
+    }
+}
