555f510a35bcede362cbe7735bedbb31397d27f3
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2018-11-26 14:30:38 +0200

    initial

---
+++ 02.Data Types and Variables - Lab/P02.CircleArea/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace P02.CircleArea
+{
+    using System;
+    class Startup
+    {
+        public static void Main()
+        {
+            double radius = double.Parse(Console.ReadLine());
+            double area = Math.PI * radius * radius;
+            Console.WriteLine($"{area:f12}");
+        }
+    }
+}
