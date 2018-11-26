1f9f0a6d97d9e111d7d7c56f1783362c6e95a060
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2018-11-26 15:23:48 +0200

    initial

---
+++ 03.Arrays - Exercise/P03.ZigZagArrays/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace P03.ZigZagArrays
+{
+    using System;
+    using System.Linq;
+    class Startup
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] input = new string[n];
+            string[] firstArr = new string[n];
+            string[] secondArr = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                input = Console.ReadLine().Split().ToArray();
+                if (i % 2 != 0)
+                {
+                    firstArr[i] = input[0];
+                    secondArr[i] = input[1];
+                }
+                else if (i % 2 == 0)
+                {
+                    firstArr[i] = input[1];
+                    secondArr[i] = input[0];
+                }
+            }
+            Console.WriteLine(string.Join(" ", secondArr));
+            Console.WriteLine(string.Join(" ", firstArr));
+        }
+    }
+}
