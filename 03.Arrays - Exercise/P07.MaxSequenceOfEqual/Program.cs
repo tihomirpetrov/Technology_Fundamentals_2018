aed2b39d256e164ce7a3b8b74442780787fcde92
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2018-11-26 15:32:25 +0200

    initial

---
+++ 03.Arrays - Exercise/P07.MaxSequenceOfEqual/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace P07.MaxSequenceOfEqualElements
+{
+    using System;
+    using System.Linq;
+    class StartUp
+    {
+        static void Main()
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int maxSum = 0;
+            int maxValue = 0;
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int counter = 1;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        counter++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                if (maxSum < counter)
+                {
+                    maxSum = counter;
+                    maxValue = arr[i];
+                }
+            }
+            for (int i = 0; i < maxSum; i++)
+            {
+                Console.Write($"{maxValue} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
