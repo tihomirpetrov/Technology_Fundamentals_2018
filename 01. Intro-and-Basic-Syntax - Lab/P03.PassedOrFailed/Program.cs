51deeea3f1700d9157775ca4a23927938f133856
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2018-11-26 11:18:51 +0200

    initial

---
+++ 01. Intro-and-Basic-Syntax - Lab/P03.PassedOrFailed/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace P03.PassedOrFailed
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+            if (grade <= 2.99)
+            {
+                Console.WriteLine("Failed!");
+            }
+            else
+            {
+                Console.WriteLine("Passed!");
+            }
+        }
+    }
+}
