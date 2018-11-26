5e297dd82ed02b1edefe41476c9254d3f7a7fd5b
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2018-11-26 15:17:21 +0200

    initial

---
+++ 03.Arrays - Exercise/P01.Train/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace P01.Train
+{
+    using System;
+    using System.Linq;
+
+    class Startup
+    {
+        public static void Main()
+        {
+            int countOfWagons = int.Parse(Console.ReadLine());
+
+            int[] peopleInWagons = new int[countOfWagons];
+
+            for (int i = 0; i < countOfWagons; i++)
+            {
+                peopleInWagons[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(string.Join(" ", peopleInWagons));
+            Console.WriteLine(peopleInWagons.Sum());
+        }
+    }
+}
