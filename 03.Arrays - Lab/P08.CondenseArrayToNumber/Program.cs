00f77495dd654ed8c46e1cdff555b9efc9e8d708
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2018-11-26 16:08:41 +0200

    initial

---
+++ 03.Arrays - Lab/P08.CondenseArrayToNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace P08.CondenseArrayToNumber
+{
+    using System;
+    using System.Linq;
+    class Startup
+    {
+        public static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            var counter = numbers.Length;
+            while (counter > 1)
+            {
+                for (int i = 0; i < numbers.Length - 1; i++)
+                {
+                    numbers[i] = numbers[i] + numbers[i + 1];
+                }
+                counter--;
+            }
+            Console.WriteLine(numbers[0]);
+        }
+    }
+}
