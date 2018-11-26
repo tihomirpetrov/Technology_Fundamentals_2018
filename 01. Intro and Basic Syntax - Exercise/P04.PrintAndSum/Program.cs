c8f8238c9430333c626767bfab12a85f629fd12d
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2018-11-26 10:43:44 +0200

    initial

---
+++ 01. Intro and Basic Syntax - Exercise/P04.PrintAndSum/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace P04.PrintAndSum
+{
+    using System;
+    class Startup
+    {
+        public static void Main()
+        {
+            int startNum = int.Parse(Console.ReadLine());
+            int endNum = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = startNum; i <= endNum; i++)
+            {
+                Console.Write(i + " ");
+                sum += i;
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
