4dff78bc4f6ec77be1a559228b2b0e52f87f0a81
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2018-11-26 15:10:49 +0200

    initial

---
+++ 02.Data Types and Variables - Exercise/P08.Snowballs/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace P08.Snowballs
+{
+    using System;
+    class Startup
+    {
+        public static void Main()
+        {
+            int numberOfSnowBalls = int.Parse(Console.ReadLine());
+            int snowballSnow = 0;
+            int snowballTime = 0;
+            int snowballQuality = 0;
+            //int snowballValue = 0;
+            double highestSnow = double.MinValue;
+            double highestTime = double.MinValue;
+            double highestQuality = double.MinValue;
+            double highestValue = double.MinValue;
+            for (int i = 1; i <= numberOfSnowBalls; i++)
+            {
+                snowballSnow = int.Parse(Console.ReadLine());
+                snowballTime = int.Parse(Console.ReadLine());
+                snowballQuality = int.Parse(Console.ReadLine());
+                double snowballValue = Math.Pow((snowballSnow / snowballTime), snowballQuality);
+                if (snowballValue > highestValue)
+                {
+                    highestValue = snowballValue;
+                    highestSnow = snowballSnow;
+                    highestTime = snowballTime;
+                    highestQuality = snowballQuality;
+                }
+            }
+            Console.WriteLine($"{highestSnow} : {highestTime} = {highestValue} ({highestQuality})");
+        }
+    }
+}
