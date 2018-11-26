bb38e6d1e0f853d99dd72e0d930f0e4adc12e468
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2018-11-26 16:00:40 +0200

    initial

---
+++ 03.Arrays - Lab/P04.ReverseArrayOfStrings/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace P04.ReverseArrayOfStrings
+{
+    using System;
+    using System.Linq;
+
+    class Startup
+    {
+        public static void Main()
+        {
+            string[] input = Console.ReadLine().Split().ToArray();
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{input[i]} ");
+            }
+        }
+    }
+}
---
+++ 03.Arrays - Lab/P05.SumEvenNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace P05.SumEvenNumbers
+{
+    using System;
+    using System.Linq;
+    class Startup
+    {
+        public static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    sum += numbers[i];
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
