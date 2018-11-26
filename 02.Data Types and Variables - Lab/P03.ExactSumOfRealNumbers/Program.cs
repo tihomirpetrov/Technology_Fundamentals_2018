99dbecfdcf73e5036072bb0383dbbc037a6ac85c
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2018-11-26 14:34:29 +0200

    initial

---
+++ 02.Data Types and Variables - Lab/P03.ExactSumOfRealNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace P03.ExactSumOfRealNumbers
+{
+    using System;
+    class Startup
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            decimal sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                decimal number = decimal.Parse(Console.ReadLine());
+                sum += number;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ 02.Data Types and Variables - Lab/P04.Elevator/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace P04.Elevator
+{
+    using System;
+    class Startup
+    {
+        public static void Main()
+        {
+            int peopleCount = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+            int counter = 0;
+            for (int i = 0; i < peopleCount; i++)
+            {
+                if (i % capacity == 0)
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
