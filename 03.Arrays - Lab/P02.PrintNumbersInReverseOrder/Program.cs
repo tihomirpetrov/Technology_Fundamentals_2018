8fe5aeda16ba7a476415558a9b16f09b89369dd7
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2018-11-26 15:51:11 +0200

    initial

---
+++ 03.Arrays - Lab/P02.PrintNumbersInReverseOrder/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace P02.PrintNumbersInReverseOrder
+{
+    using System;
+
+    class Startup
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] numbers = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = numbers.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{numbers[i]} ");
+            }
+        }
+    }
+}
---
+++ 03.Arrays - Lab/P03.RoundingNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace P03.RoundingNumbers
+{
+    using System;
+    using System.Linq;
+    class Startup
+    {
+        public static void Main()
+        {
+            string[] realNumbers = Console.ReadLine().Split().ToArray();
+            double[] roundNumbers = new double[realNumbers.Length];
+            for (int i = 0; i < realNumbers.Length; i++)
+            {
+                roundNumbers[i] = double.Parse(realNumbers[i]);
+            }
+            for (int i = 0; i < realNumbers.Length; i++)
+            {
+                Console.WriteLine($"{roundNumbers[i]} => {Math.Round(roundNumbers[i], 0, MidpointRounding.AwayFromZero)}");
+            }
+        }
+    }
+}
