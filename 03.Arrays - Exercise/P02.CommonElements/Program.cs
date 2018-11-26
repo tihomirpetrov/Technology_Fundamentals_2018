daa5c104dc484346b51758361520ee62e9099fc4
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2018-11-26 15:21:11 +0200

    initial

---
+++ 03.Arrays - Exercise/P02.CommonElements/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace P02.CommonElements
+{
+    using System;
+    using System.Linq;
+
+    class Startup
+    {
+        public static void Main()
+        {
+            string[] firstInput = Console.ReadLine().Split().ToArray();
+            string[] secondInput = Console.ReadLine().Split().ToArray();
+
+            for (int i = 0; i < secondInput.Length; i++)
+            {
+                for (int j = 0; j < firstInput.Length; j++)
+                {
+                    if (firstInput[j] == secondInput[i])
+                    {
+                        Console.Write(secondInput[i] + ' ');
+                    }
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
