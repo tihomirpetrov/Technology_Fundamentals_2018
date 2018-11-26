46dd62b2f25a3f14b08c34ddb9e1d6b47c357426
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2018-11-26 11:26:34 +0200

    initial

---
+++ 01. Intro-and-Basic-Syntax - Lab/P04.BackIn30Minutes/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace P04.BackIn30Minutes
+{
+    using System;
+    class Startup
+    {
+        public static void Main()
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            int addMinutes = minutes + 30;
+
+            if (addMinutes <= 59)
+            {
+                Console.WriteLine($"{hours}:{addMinutes}");
+            }
+            else if (addMinutes > 59)
+            {
+                hours = hours + 1;
+                addMinutes = addMinutes - 60;
+                if (hours > 23)
+                {
+                    hours = 0;
+                }
+                if (addMinutes < 10)
+                {
+                    Console.WriteLine($"{hours}:0{addMinutes}");
+                }
+                else
+                {
+                    Console.WriteLine($"{hours}:{addMinutes}");
+                }
+            }
+        }
+    }
+}
