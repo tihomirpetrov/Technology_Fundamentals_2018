9c7004ed29e74c61c0b5488a62ea533749122258
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2018-11-26 14:36:35 +0200

    initial

---
+++ 02.Data Types and Variables - Lab/P05.CenturiesToMinutes/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace P05.CenturiesToMinutes
+{
+    using System;
+    class Startup
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int years = n * 100;
+            int days = (int)(years * 365.2422);
+            int hours = days * 24;
+            int minutes = hours * 60;
+
+            Console.WriteLine($"{n} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes");
+        }
+    }
+}
---
+++ 02.Data Types and Variables - Lab/P06.SpecialNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace P06.SpecialNumbers
+{
+    using System;
+    class Startup
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                int sum = 0;
+                int number = i;
+                while (number != 0)
+                {
+                    int lastDigit = number % 10;
+                    number /= 10;
+                    sum += lastDigit;
+
+                }
+                bool isSpecial = (sum == 5 || sum == 7 || sum == 11);
+
+                Console.WriteLine($"{i} -> {isSpecial}");
+            }
+        }
+    }
+}
