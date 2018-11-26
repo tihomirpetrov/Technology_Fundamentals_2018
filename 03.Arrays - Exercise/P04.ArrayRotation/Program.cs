2f478d6ee54a574969c410039e77c447b1657294
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2018-11-26 15:25:01 +0200

    initial

---
+++ 03.Arrays - Exercise/P04.ArrayRotation/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace P04.ArrayRotation
+{
+    using System;
+    using System.Linq;
+    class Startup
+    {
+        public static void Main()
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int firstNumber = input[0];
+                for (int j = 0; j < input.Length - 1; j++)
+                {
+                    input[j] = input[j + 1];
+                }
+                input[input.Length - 1] = firstNumber;
+            }
+
+            Console.WriteLine(string.Join(" ", input));
+        }
+    }
+}
