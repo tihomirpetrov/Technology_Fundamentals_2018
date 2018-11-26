d264d825945587dda0b4c1168c003fa6281fb0ae
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2018-11-26 15:04:37 +0200

    initial

---
+++ 02.Data Types and Variables - Exercise/P06.ReversedChars/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace P06.ReversedChars
+{
+    using System;
+    class Startup
+    {
+        public static void Main()
+        {
+            string firstLetter = Console.ReadLine();
+            string secondLetter = Console.ReadLine();
+            string thirdLetter = Console.ReadLine();
+
+            Console.WriteLine($"{thirdLetter} {secondLetter} {firstLetter}");
+        }
+    }
+}
---
+++ 02.Data Types and Variables - Exercise/P07.LowerToUpper/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace P07.LowerToUpper
+{
+    using System;
+    class Startup
+    {
+        public static void Main()
+        {
+            char letter = char.Parse(Console.ReadLine());
+
+            if (char.IsLower(letter))
+            {
+                Console.WriteLine("lower-case");
+            }
+            else
+            {
+                Console.WriteLine("upper-case");
+            }
+        }
+    }
+}
