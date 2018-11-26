27add59fe435fd6f699b5d5db489fb1434bfa1c2
tihomirpetrov <tihomirpetrov79@gmail.com>
2018-11-26 19:21:22 +0200

    initial commit

---
+++ 04.Methods - More Exercises/P01.SignOfIntegerNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace P01.SignOfIntegerNumbers
+{
+    using System;
+    class Startup
+    {
+        public static void Main()
+        {
+            SignOfIntegerNumbers();
+        }
+
+        private static void SignOfIntegerNumbers()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            if (n > 0)
+            {
+                Console.WriteLine($"The number {n} is positive.");
+            }
+            else if (n < 0)
+            {
+                Console.WriteLine($"The number {n} is negative.");
+            }
+            else if (n == 0)
+            {
+                Console.WriteLine($"The number {n} is zero.");
+            }
+        }
+    }
+}
