ec29d2cf3962aa37c5d39ba60a86161bc8b00fff
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2018-11-26 16:04:21 +0200

    initial

---
+++ 03.Arrays - Lab/P06.EvenAndOddSubtraction/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace P06.EvenAndOddSubtraction
+{
+    using System;
+    using System.Linq;
+    class Startup
+    {
+        public static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sumEven = 0;
+            int sumOdd = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    sumEven += numbers[i];
+                }
+                else
+                {
+                    sumOdd += numbers[i];
+                }
+            }
+            int result = sumEven - sumOdd;
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 03.Arrays - Lab/P07.EqualArrays/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace P07.EqualArrays
+{
+    using System;
+    using System.Linq;
+
+    class Startup
+    {
+        public static void Main()
+        {
+            int[] firstNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] secondNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sum = 0;
+            bool isTrue = false;
+            for (int i = 0; i < firstNumbers.Length; i++)
+            {
+                if (firstNumbers[i] != secondNumbers[i])
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    isTrue = true;
+                    break;
+                }
+                else
+                {
+                    sum += firstNumbers[i];
+                }
+            }
+
+            if (!isTrue)
+            {
+                Console.WriteLine($"Arrays are identical. Sum: {sum}");
+            }
+        }
+    }
+}
