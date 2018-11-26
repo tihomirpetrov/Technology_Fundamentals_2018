09d08d2e45c70d6edb7efcd6443d1a1469bee1a9
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2018-11-26 10:57:08 +0200

    initial

---
+++ 01. Intro and Basic Syntax - Exercise/P09.PadawanEquipment/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace P09.PadawanEquipment
+{
+    using System;
+    class Startup
+    {
+        public static void Main()
+        {
+            double amountOfMoney = double.Parse(Console.ReadLine());
+            int countOfStudents = int.Parse(Console.ReadLine());
+            double priceSabres = double.Parse(Console.ReadLine());
+            double priceRobes = double.Parse(Console.ReadLine());
+            double priceBelts = double.Parse(Console.ReadLine());
+            double tenPercent = Math.Ceiling(countOfStudents * 1.1);
+            double sabresTotal = (priceSabres * tenPercent);
+            double robesTotal = priceRobes * countOfStudents;
+            int freeBelts = 0;
+            for (int i = 1; i <= countOfStudents; i++)
+            {
+                if (i % 6 == 0)
+                {
+                    freeBelts++;
+                }
+            }
+            double beltsTotal = priceBelts * (countOfStudents - freeBelts);
+
+            double totalPriceForEquipment = sabresTotal + robesTotal + beltsTotal;
+            if (amountOfMoney >= totalPriceForEquipment)
+            {
+                Console.WriteLine($"The money is enough - it would cost {totalPriceForEquipment:f2}lv.");
+            }
+            else
+            {
+                double neededMoney = Math.Abs(totalPriceForEquipment - amountOfMoney);
+                Console.WriteLine($"Ivan Cho will need {neededMoney:f2}lv more.");
+            }
+        }
+    }
+}
---
+++ 01. Intro and Basic Syntax - Exercise/P10.RageExpanses/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace P10.Rage_Expenses
+{
+    using System;
+
+    class Startup
+    {
+        public static void Main()
+        {
+            int lostGameCount = int.Parse(Console.ReadLine());
+            double headsetPrice = double.Parse(Console.ReadLine());
+            double mousePrice = double.Parse(Console.ReadLine());
+            double keyboardPrice = double.Parse(Console.ReadLine());
+            double displayPrice = double.Parse(Console.ReadLine());
+            int counterHeadset = 0;
+            int counterMouse = 0;
+            int counterKeyboard = 0;
+            int counterDisplay = 0;
+
+            for (int i = 1; i <= lostGameCount; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    counterHeadset++;
+                }
+                if (i % 3 == 0)
+                {
+                    counterMouse++;
+                }
+                if (i % 2 == 0 && i % 3 == 0)
+                {
+                    counterKeyboard++;
+                    if (counterKeyboard % 2 == 0)
+                    {
+                        counterDisplay++;
+                    }
+                }
+            }
+            double totalHeadset = headsetPrice * counterHeadset;
+            double totalMouse = mousePrice * counterMouse;
+            double totalKeyboard = keyboardPrice * counterKeyboard;
+            double totalDisplay = displayPrice * counterDisplay;
+            double totalexpanses = totalHeadset + totalMouse + totalKeyboard + totalDisplay;
+            Console.WriteLine($"Rage expenses: {totalexpanses:f2} lv.");
+        }
+    }
+}
