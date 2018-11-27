553a04826bec556cd46b19037f8620d898be1fcd
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2018-11-27 09:06:30 +0200

    initial commit

---
+++ 06.Dictionaries, Lambda and LINQ - Lab/P01.CountRealNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace P01.CountRealNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    class Startup
+    {
+        static void Main()
+        {
+            List<double> numbers = Console.ReadLine().Split().Select(double.Parse).ToList();
+            SortedDictionary<double, int> counts = new SortedDictionary<double, int>();
+
+            foreach (var number in numbers)
+            {
+                if (counts.ContainsKey(number))
+                {
+                    counts[number]++;
+                }
+                else
+                {
+                    counts[number] = 1;
+                }
+            }
+
+            foreach (var number in counts.Keys)
+            {
+                Console.WriteLine($"{number} -> {counts[number]}");
+            }
+        }
+    }
+}
---
+++ 06.Dictionaries, Lambda and LINQ - Lab/P02.OddOccurrences/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace P02.OddOccurrences
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Startup
+    {
+        public static void Main()
+        {
+            string[] words = Console.ReadLine().Split();
+            Dictionary<string, int> counts = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                string wordInLowerCase = word.ToLower();
+                if (counts.ContainsKey(wordInLowerCase))
+                {
+                    counts[wordInLowerCase]++;
+                }
+                else
+                {
+                    counts.Add(wordInLowerCase, 1);
+                }
+            }
+
+            foreach (var count in counts)
+            {
+                if (count.Value % 2 != 0)
+                {
+                    Console.Write(count.Key + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
