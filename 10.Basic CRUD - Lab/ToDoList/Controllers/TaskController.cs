9c7d0aef4c45b19a2fd07696878fb91a415622b1
tihomirpetrov <tihomirpetrov79@gmail.com>
2018-11-29 23:34:36 +0200

    initial commit

---
+++ 10.Basic CRUD - Lab/ToDoList/Controllers/TaskController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using ToDoList.Data;
+using ToDoList.Models;
+
+
+namespace ToDoList.Controllers
+{
+    public class TaskController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            using (var db = new ToDoDbContext())
+            {
+                var allTasks = db.Tasks.ToList();
+                return View(allTasks);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(string title, string comments)
+        {
+            if (string.IsNullOrEmpty(title) || string.IsNullOrEmpty(comments))
+            {
+                return RedirectToAction("Index");
+            }
+
+            Task task = new Task
+            {
+                Title = title,
+                Comments = comments
+            };
+
+            using (var db = new ToDoDbContext())
+            {
+                db.Tasks.Add(task);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            using (var db = new ToDoDbContext())
+            {
+                var taskToEdit = db.Tasks.FirstOrDefault(t => t.Id == id);
+                if (taskToEdit == null)
+                {
+                    return RedirectToAction("Index");
+                }
+                return View(taskToEdit);
+            }   
+        }
+
+        [HttpPost]
+
+        public IActionResult Edit(Task task)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            using (var db = new ToDoDbContext())
+            {
+                var taskToEdit = db.Tasks.FirstOrDefault(t => t.Id == task.Id);
+                taskToEdit.Title = task.Title;
+                taskToEdit.Comments = task.Comments;
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ 10.Basic CRUD - Lab/ToDoList/Models/Task.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoList.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Comments { get; set; }
+    }
+}
