08cbe9aaf8e97014364c9570a57fd312196b52dd
tihomirpetrov <tihomirpetrov79@gmail.com>
2018-12-02 16:17:13 +0200

    final exam preparation

---
+++ Final Exam/P01/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace P01
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<string, List<string>> wordDefinitions = new Dictionary<string, List<string>>();
+            string[] input = Console.ReadLine().Split(" | ");
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string[] line = input[i].Split(": ");
+                string definition = line[1];
+                string word = line[0];
+
+                if (!wordDefinitions.ContainsKey(word))
+                {
+                    wordDefinitions.Add(word, new List<string>());
+                }
+                wordDefinitions[word].Add(definition);
+            }
+
+            string[] input2 = Console.ReadLine().Split(" | ");
+
+            for (int i = 0; i < input2.Length; i++)
+            {
+                string name = input2[i];
+
+                foreach (var def in wordDefinitions)
+                {
+                    if (def.Key == name)
+                    {
+                        Console.WriteLine(def.Key);
+                        foreach (var item in def.Value.OrderByDescending(x => x.Length))
+                        {
+                            Console.WriteLine($" -{item}");
+                        }
+                    }
+                }
+            }
+
+            string input3 = Console.ReadLine();
+
+            if (input3 == "List")
+            {
+                foreach (var def in wordDefinitions.OrderBy(x => x.Key))
+                {
+                    Console.Write(def.Key + " ");
+                }
+                Console.WriteLine();
+            }
+
+            //if (input3 == "End")
+            //{
+            //    return;
+            //}
+        }
+    }
+}
---
+++ Final Exam/P02/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace P02
+{
+    using System;
+    using System.Text;
+    class Program
+    {
+        static void Main()
+        {
+            string firstInput = Console.ReadLine();
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < firstInput.Length; i++)
+            {
+                if (!(firstInput[i] >= 100 && firstInput[i] <= 122 || firstInput[i] == ',' || firstInput[i] == '|' || firstInput[i] == '#'))
+                {
+                    Console.WriteLine("This is not the book you are looking for.");
+                    return;
+                }
+            }
+
+            for (int i = 0; i < firstInput.Length; i++)
+            {
+                sb.Append((char)(firstInput[i] - 3));
+            }
+
+            string[] secondInput = Console.ReadLine().Split();
+            string forReplacement = secondInput[0];
+            string replacement = secondInput[1];
+
+            for (int i = 0; i < sb.Length; i++)
+            {
+                sb = sb.Replace(forReplacement, replacement);
+            }
+            //string text = sb.ToString();
+            //sb.Clear();
+            //string[] temp = text.Split(forReplacement);
+
+            //for (int i = 0; i < temp.Length - 1; i++)
+            //{
+            //    sb.Append(temp[i]);
+            //    sb.Append(replacement);
+            //}
+            //sb.Append(temp[temp.Length - 1]);
+
+            Console.WriteLine(sb);
+        }
+    }
+}
