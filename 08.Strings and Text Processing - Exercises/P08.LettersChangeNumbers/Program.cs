1d86ee71bc6b5a63e3d36b993e77400ee07a395a
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2018-12-18 09:52:25 +0200

    initial commit

---
+++ 08.Strings and Text Processing - Exercises/P08.LettersChangeNumbers/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace P08.LettersChangeNumbers
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+        public static void Main()
+        {
+            string[] input = Console.ReadLine().Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+            double sum = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                char[] chars = input[i].ToCharArray();
+                char firstLetter = chars[0];
+                char lastLetter = chars[chars.Length - 1];
+                string numberStr = string.Empty;
+                double number = 0;
+                int divide = 0;
+                int multiply = 0;
+                int add = 0;
+                int substract = 0;
+
+                for (int j = 1; j < chars.Length - 1; j++)
+                {
+                    numberStr += chars[j];
+                }
+                number = double.Parse(numberStr);
+
+                if (firstLetter >= 65 && firstLetter <= 90)
+                {
+                    divide = firstLetter - 64;
+                    number = number / divide;
+                }
+                else if (firstLetter >= 97 && firstLetter <= 122)
+                {
+                    multiply = firstLetter - 96;
+                    number = number * multiply;
+                }
+
+
+                if (lastLetter >= 65 && lastLetter <= 90)
+                {
+                    substract = lastLetter - 64;
+                    number = number - substract;
+                }
+                else if (lastLetter >= 97 && lastLetter <= 122)
+                {
+                    add = lastLetter - 96;
+                    number = number + add;
+                }
+
+                sum += number;
+
+            }
+            Console.WriteLine($"{sum:f2}");
+
+        }
+    }
+}
---
+++ 08.Strings and Text Processing - Exercises/P09.RageQuit/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace P09.RageQuit
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    class StartUp
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine().ToUpper();
+            List<string> textWithSeparateDigits = new List<string>();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in input)
+            {
+                if ((int)item < 48 || (int)item > 57)
+                {
+                    sb.Append(item);
+                }
+                else
+                {
+                    if (sb.Length > 0)
+                    {
+                        textWithSeparateDigits.Add(sb.ToString());
+                        sb.Clear();
+                    }
+                    textWithSeparateDigits.Add(item.ToString());
+                }
+            }
+
+            List<string> text = new List<string>();
+            foreach (var item in textWithSeparateDigits)
+            {
+                if (item.Length == 1)
+                {
+                    if (item == "0" || item == "1" || item == "2" || item == "3" || item == "4" || item == "5" || item == "6" || item == "7" || item == "8" || item == "9")
+                    {
+                        sb.Append(item);
+                    }
+                    else
+                    {
+                        if (sb.Length > 0)
+                        {
+                            text.Add(sb.ToString());
+                        }
+                        sb.Clear();
+                        text.Add(item.ToString());
+                    }
+                }
+                else
+                {
+                    if (sb.Length > 0)
+                    {
+                        text.Add(sb.ToString());
+                    }
+                    sb.Clear();
+                    text.Add(item);
+                }
+            }
+            text.Add(sb.ToString());
+            sb.Clear();
+
+            for (int i = 0; i < text.Count; i += 2)
+            {
+                string word = text[i];
+                int count = int.Parse(text[i + 1]);
+
+                for (int j = 0; j < count; j++)
+                {
+                    sb.Append(word);
+                }
+            }
+            string message = sb.ToString();
+
+            List<char> symbols = new List<char>();
+            foreach (var item in message)
+            {
+                if (!(symbols.Contains(item)))
+                {
+                    symbols.Add(item);
+                }
+            }
+            Console.WriteLine($"Unique symbols used: {symbols.Count}");
+            Console.WriteLine(message);
+        }
+    }
+}
