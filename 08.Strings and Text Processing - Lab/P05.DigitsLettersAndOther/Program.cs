c8685340cae351a07572b5090a1ddb5cc09d7b18
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2018-12-18 13:29:08 +0200

    initial commit

---
+++ 08.Strings and Text Processing - Lab/P05.DigitsLettersAndOther/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace P05.DigitsLettersAndOther
+{
+    using System;
+
+    class Program
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            string numbers = string.Empty;
+            string letters = string.Empty;
+            string other = string.Empty;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] >= 48 && input[i] <= 57)
+                {
+                    numbers += input[i];
+                }
+                else if (input[i] >= 65 && input[i] <= 90 || input[i] >= 97 && input[i] <= 122)
+                {
+                    letters += input[i];
+                }
+                else
+                {
+                    other += input[i];
+                }
+            }
+            Console.WriteLine(numbers);
+            Console.WriteLine(letters);
+            Console.WriteLine(other);
+        }
+    }
+}
