076aa477b996dd7093f6c762dbe7f98e68fc4678
tihomirpetrov <tihomirpetrov79@gmail.com>
2018-12-19 22:42:01 +0200

    added missing problems

---
+++ 07.Objects and Classes - Lab/P05.Students/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace P05.Students
+{
+    using System;
+    using System.Collections.Generic;
+    class Program
+    {
+        public static void Main()
+        {
+            List<Student> students = new List<Student>();
+
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                string[] tokens = input.Split();
+                string firstName = tokens[0];
+                string lastName = tokens[1];
+                int age = int.Parse(tokens[2]);
+                string city = tokens[3];
+
+                Student student = new Student();
+                {
+                    student.FirstName = firstName;
+                    student.LastName = lastName;
+                    student.Age = age;
+                    student.City = city;
+                };
+
+                students.Add(student);
+
+                input = Console.ReadLine();
+            }
+
+            string filterCity = Console.ReadLine();
+
+            foreach (Student student in students)
+            {
+                if (student.City == filterCity)
+                {
+                    Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+                }
+            }
+        }
+    }
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+    }
+}
