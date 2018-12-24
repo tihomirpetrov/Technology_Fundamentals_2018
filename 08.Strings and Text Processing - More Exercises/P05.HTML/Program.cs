c0788f63f2ebe36627a3cbc57735667f197eccb2
tihomirpetrov <tihomirpetrov79@gmail.com>
2018-12-24 13:09:27 +0200

    added missing problems

--- 08.Strings and Text Processing - More Exercises/P05.HTML/Program.cs
+++ 08.Strings and Text Processing - More Exercises/P05.HTML/Program.cs
@@ -1,11 +1,40 @@
 ﻿namespace P05.HTML
 {
     using System;
+    using System.Collections.Generic;
+
     class Program
     {
         public static void Main()
         {
+            string articleTitle = Console.ReadLine();
+            string articleContent = Console.ReadLine();
+            string command = Console.ReadLine();
+            List<string> comments = new List<string>();
 
+            while (command != "end of comments")
+            {
+                if (!comments.Contains(command))
+                {
+                    comments.Add(command);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine("<h1>");
+            Console.WriteLine($"{articleTitle}");
+            Console.WriteLine("</h1>");
+            Console.WriteLine("<article>");
+            Console.WriteLine($"{articleContent}");
+            Console.WriteLine("</article>");
+
+            foreach (var item in comments)
+            {
+                Console.WriteLine("<div>");
+                Console.WriteLine($"{item}");
+                Console.WriteLine("</div>");
+            }
         }
     }
 }
