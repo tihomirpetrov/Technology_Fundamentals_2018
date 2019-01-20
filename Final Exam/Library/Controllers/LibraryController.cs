ce4c0950b8290437fa77d88c3e6ba2e1b40d65f6
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-01-20 19:07:45 +0200

    added missing

---
+++ Final Exam/Library/Controllers/LibraryController.cs
@@ -0,0 +1,126 @@
+﻿namespace Library.Controllers
+{
+    using Library.Data;
+    using Library.Models;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Collections.Generic;
+    using System.Linq;
+    public class LibraryController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            using (LibraryDbContext db = new LibraryDbContext())
+            {
+                List<Book>books = db.Books.ToList();
+                return View(books);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(string title, string author, double price)
+        {
+            if (string.IsNullOrEmpty(title) || string.IsNullOrEmpty(author))
+            {
+                return RedirectToAction("Index");
+            }
+
+            Book book = new Book
+            {
+                Title = title,
+                Author = author,
+                Price = price
+            };
+
+            using (LibraryDbContext db = new LibraryDbContext())
+            {
+                db.Books.Add(book);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            using (LibraryDbContext db = new LibraryDbContext())
+            {
+                Book oldBook = db.Books.FirstOrDefault(x => x.Id == id);
+                if (oldBook == null)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                return View(oldBook);
+            }
+
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Book newBook)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            using (LibraryDbContext db = new LibraryDbContext())
+            {
+                Book oldBook = db.Books.FirstOrDefault(x => x.Id == newBook.Id);
+                if (oldBook == null)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                oldBook.Title = newBook.Title;
+                oldBook.Author = newBook.Author;
+                oldBook.Price = newBook.Price;
+
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            using (LibraryDbContext db = new LibraryDbContext())
+            {
+                Book oldBook = db.Books.FirstOrDefault(x => x.Id == id);
+                if (oldBook == null)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                return View(oldBook);
+            }
+
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Book book)
+        {
+            using (LibraryDbContext db = new LibraryDbContext())
+            {
+                if (book == null)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                db.Books.Remove(book);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+        }
+    }
+}
---
+++ Final Exam/Library/Data/LibraryDbContext.cs
@@ -0,0 +1,16 @@
+﻿namespace Library.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Library.Models;
+    public class LibraryDbContext : DbContext
+    {
+        private const string ConnectionString = @"Server=.\SQLEXPRESS;Database=LibraryDb;Integrated security=True";
+        public virtual DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+
+    }
+}
